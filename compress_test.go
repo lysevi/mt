6eafafebaab20bc2ba15e9a8b9b4e5174c7903ef
Sergey Lyubimov <lysevi@gmail.com>
2016-02-15 21:52:26 +0400

    compression integration test.

--- compress_test.go
+++ compress_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"testing"
 )
 
@@ -667,6 +668,35 @@ func TestCompressMeas(t *testing.T) {
 	for i, v := range meases {
 		if v.Id != 1 || v.Flg != Flag(i) || v.Tstamp != Time(i) || v.Value != int64(i) {
 			t.Errorf("meas read error: ", v.String())
+		}
+	}
+}
+
+func TestCompressedBlockIntegration(t *testing.T) {
+	cblock := NewCompressedBlock()
+	iterations := 5000
+	tm := Time(1)
+	meases := []Meas{}
+	for i := 0; i < iterations; i++ {
+		m := NewMeas(1, tm, int64(math.Sin(float64(i))), Flag(0x002202))
+		tm += Time(1000)
+		meases = append(meases, m)
+	}
+
+	cblock.Add_range(meases)
+
+	uncompressedSize := uint64(28 * iterations)
+	if cblock.byteNum >= uncompressedSize {
+		t.Error("compression not work: ", cblock.byteNum, uncompressedSize)
+	}
+	//	fmt.Println("used bytes: ", cblock.byteNum)
+	//	fmt.Println("full size: ", 28*iterations)
+	//	fmt.Println("count: ", len(meases))
+
+	readed := cblock.ReadAll()
+	for i, _ := range meases {
+		if !measEqual(readed[i], meases[i]) {
+			t.Error("i: ", i, readed[i].String(), meases[i].String())
 		}
 	}
 }
--- main.go
+++ main.go
@@ -2,20 +2,8 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
 	fmt.Println("****************")
-	cblock := NewCompressedBlock()
-
-	iterations := 1000
-	for i := 0; i < iterations; i++ {
-		t := Time(time.Millisecond * time.Duration(i))
-		m := NewMeas(1, t, int64(i), Flag(i%2))
-		cblock.Add(m)
-	}
-
-	fmt.Println("used bytes: ", cblock.byteNum)
-	fmt.Println("full size: ", 28*iterations)
 }
