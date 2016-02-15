3247ad9fe61750e2fa02e612872f3699713d9fef
Sergey Lyubimov <lysevi@gmail.com>
2016-02-15 07:26:02 +0300

    benchmarks refact.

--- becnhsmarks_test.go
+++ becnhsmarks_test.go
@@ -7,8 +7,6 @@ import (
 func BenchmarkTimeDeltas64(b *testing.B) {
 	cblock := NewCompressedBlock()
 	for i := 0; i < b.N; i++ {
-		cblock.delta_64(1)
-		cblock.delta_64(64)
 		cblock.delta_64(63)
 	}
 }
@@ -16,9 +14,7 @@ func BenchmarkTimeDeltas64(b *testing.B) {
 func BenchmarkTimeDeltas256(b *testing.B) {
 	cblock := NewCompressedBlock()
 	for i := 0; i < b.N; i++ {
-		cblock.delta_256(256)
 		cblock.delta_256(255)
-		cblock.delta_256(65)
 	}
 }
 
@@ -26,17 +22,12 @@ func BenchmarkTimeDeltas2048(b *testing.B) {
 	cblock := NewCompressedBlock()
 	for i := 0; i < b.N; i++ {
 		cblock.delta_2048(2048)
-		cblock.delta_2048(257)
-		cblock.delta_2048(4095)
 	}
 }
 
 func BenchmarkTimeDeltas_big(b *testing.B) {
 	cblock := NewCompressedBlock()
 	for i := 0; i < b.N; i++ {
-		cblock.delta_big(2049)
-		cblock.delta_big(65535)
-		cblock.delta_big(4095)
 		cblock.delta_big(4294967295)
 	}
 }
@@ -67,21 +58,13 @@ func BenchmarkDeltaTimeRW(b *testing.B) {
 		cblock := NewCompressedBlock()
 		iterations := 1
 		for i := 0; i < iterations; i++ {
-			cblock.write_64(cblock.delta_64(1))
-			cblock.write_64(cblock.delta_64(64))
 			cblock.write_64(cblock.delta_64(64))
 
-			cblock.write_256(cblock.delta_256(256))
 			cblock.write_256(cblock.delta_256(255))
-			cblock.write_256(cblock.delta_256(65))
 
-			cblock.write_2048(cblock.delta_2048(2048))
-			cblock.write_2048(cblock.delta_2048(257))
 			cblock.write_2048(cblock.delta_2048(4095))
 
-			cblock.write_big(cblock.delta_big(2049))
 			cblock.write_big(cblock.delta_big(65535))
-			cblock.write_big(cblock.delta_big(4095))
 		}
 
 		cblock.byteNum = 0
@@ -91,19 +74,6 @@ func BenchmarkDeltaTimeRW(b *testing.B) {
 			cblock.readTime(0)
 			cblock.readTime(0)
 			cblock.readTime(0)
-
-			cblock.readTime(0)
-			cblock.readTime(0)
-			cblock.readTime(0)
-
-			cblock.readTime(0)
-			cblock.readTime(0)
-			cblock.readTime(0)
-
-			cblock.readTime(0)
-			cblock.readTime(0)
-			cblock.readTime(0)
-
 		}
 	}
 }
